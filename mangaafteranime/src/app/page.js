654486b4477a6057eaed8c61bd16a3031dac0a30
Cameron Oberlin <125579644+hipyr@users.noreply.github.com>
2024-03-19 14:21:01 -0400

    Made Carousel

--- mangaafteranime/src/app/page.js
+++ mangaafteranime/src/app/page.js
@@ -2,15 +2,17 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "../components/Navbar/Navbar";
 import MainCard from "@/components/MainCard/MainCard";
-import PopularCarousel from "@/components/PopularCarousel/PopularCarousel";
+import Carousel from "@/components/Carousel/Carousel";
 import { MantineProvider } from "@mantine/core";
 export default function Home() {
   return (
     <>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Navbar />
-      <MainCard />
-      <PopularCarousel />
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Navbar />
+        <MainCard />
+        <Carousel ranking={'bypopularity'} name={'Popular'} />
+        <Carousel ranking={'all'} name={'All Animes'} />
+        <Carousel ranking={'upcoming'} name={'Upcoming Animes'} />
       </MantineProvider>
     </>
   );
--- mangaafteranime/src/components/Carousel/Carousel.js
+++ mangaafteranime/src/components/Carousel/Carousel.js
@@ -1,11 +1,11 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import styles from './PopularCarousel.module.scss';
+import styles from './Carousel.module.scss';
 import '@mantine/core/styles.css'; // Import core styles
 import '@mantine/carousel/styles.css';
 import { Carousel } from '@mantine/carousel';
 
-export default function PopularCarousel() {
+export default function PopularCarousel({ ranking, name }) {
   const [popularAnimes, setPopularAnimes] = useState([]);
   const [error, setError] = useState(null);
 
@@ -20,7 +20,7 @@ export default function PopularCarousel() {
             'Content-Type': 'application/json',
           },
         };
-        const response = await fetch(`https://api.myanimelist.net/v2/anime/ranking?ranking_type=bypopularity&limit=20`, requestOptions);
+        const response = await fetch(`https://api.myanimelist.net/v2/anime/ranking?ranking_type=${ranking}&limit=20`, requestOptions);
         const data = await response.json();
         setPopularAnimes(data.data || []);
         setError(null);
@@ -36,13 +36,14 @@ export default function PopularCarousel() {
 
   return (
     <div className={styles['popularCarousel']}>
-    <h1>Popular</h1>
+      <h1>{name}</h1>
       {error && <p>{error}</p>}
+      <div className={styles['Carousel-Container']}></div>
       <Carousel slideSize="1%" slideGap="xs" align="start" loop withIndicators slidesToScroll={1}>
         {popularAnimes.length > 0 ? (
           popularAnimes.map((anime) => (
-            <Carousel.Slide>
-              <img src={anime.node.main_picture.medium} alt={`Cover of ${anime.node.title}`} />
+            <Carousel.Slide >
+              <img src={anime.node.main_picture.medium} alt={`Cover of ${anime.node.title}`} className={styles['Slides']} />
               {/* <p>{anime.node.title}</p> */}
             </Carousel.Slide>
           ))
